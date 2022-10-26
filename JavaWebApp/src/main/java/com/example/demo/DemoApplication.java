24b2ae3472cd499b459576414f0a9b9e2ae8b2b0
bwhit10 <bwhitfield16@gmail.com>
2022-10-25 21:12:54 -0400

    fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+    
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-	    String name = "brittany"	
-	
-        System.out.println(name);
-
+        SpringApplication.run(DemoApplication.class, args);
+        
+		String name = "vamsi";
+       
+		
+		System.out.println(name);
+    }
+}
